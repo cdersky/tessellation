a74bb82c80987ace0abed66145b5759bcb3df6e6
claire <cdersky@gmail.com>
2015-12-03 10:54:05 -0800

    collision detection and score tracking

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Draw circles in an svg element
+
 // SVG element
 var width = 960,
 height = 700;
@@ -7,82 +9,148 @@ var svg = d3.select("body").append("svg")
 .attr("height", height);
 
 // draw circles
-var dataset = [ 5, 10, 15, 20, 25 ];
+var dataset = [ 5, 10, 15, 20, 25, 5, 10, 15, 20, 25, ];
 
 var circles = svg.selectAll("circle")
 .data(dataset)
 .enter()
 .append("circle");
 
-
 circles.attr("cx", function(d, i) {
-  return (i * 50) + 25;
-        }) // Takes the reference to all circles and sets the cx attribute for each one. Our data has already been bound to the circle elements, so for each circle, the value d matches the corresponding value in our original data set (5, 10, 15, 20, or 25). Another value, i, is also automatically populated for us. i is a numeric index value of the current element. Counting starts at zero, so for our “first” circle i == 0, the second circle’s i == 1 and so on. We’re using i to push each subsequent circle over to the right, because each subsequent loop through, the value of i increases. ***To make sure i is available to your custom function, you must include it as an argument in the function definition (function(d, i)). You must also include d, even if you don’t use d within your function (as in the case above).***
-       .attr("cy", height/4) // height is the height of the entire SVG, so height/2 is one-half of its height. This has the effect of aligning all circles in the vertical center.
-       .attr("r", function(d) {
-        return d; 
-       }) //Finally, the radius r of each circle is simply set to d, the corresponding data value.
-       .attr('fill', 'white')
-       .attr('stroke', 'black')
-       .attr('stroke-width', function(d, i){
-        return d*(1/3);
-      });
+  return (i * 70) + 25;
+}) 
+.attr("cy", height/4) 
+.attr("r", function(d) {
+  return Math.abs(d); 
+}) 
+.attr('fill', 'white')
+.attr('stroke', 'black')
+.attr('stroke-width', function(d){
+  return d*(1/3);
+});
 
 
-// make circles move to a new random location
+// Make circles move to a new random location every second using transitions.
+
 var changeLocation = function() {
   circles
   .transition()
-        // .delay(2000)
-        .duration(1000)
-        .attr("cx", function(d,i) {                    
-          var num = Math.random();
-          return width*num;        
-        }) 
-        .attr("cy", function(d,i) {
-          var num = Math.random();
-          return height*num;
-        })
-        .attr("r", function(d,i) {
-          var num = Math.random();
-          return 100*num;
-        })
-        .style("opacity", function(){
-          var num = Math.random();
-          return num;
-        })
-        .ease("elastic")
+    // .delay(2000)
+    .duration(1000)
+    .attr("cx", function(d,i) {                    
+      var num = Math.random();
+      return width*num;        
+    }) 
+    .attr("cy", function(d,i) {
+      var num = Math.random();
+      return height*num;
+    })
+    .attr("r", function(d,i) {
+      var num = Math.random();
+      return Math.abs(100*num);
+    })
+    .style("opacity", function(){
+      var num = Math.random();
+      return num;
+    })
+    .ease("back"); // also try back, linear, sin, elastic, cubic, exp, quad, circle
+  };
 
-      };
-
-// make circles move to a new random location every few seconds
-var intervalID = window.setInterval(changeLocation, 2000);
+  var intervalID = window.setInterval(changeLocation, 2000);
 
 
-// a differently-colored shape
-var playerData = [10];
+// Make a differently-colored dot to represent the player. Make it draggable.
 
-var player = svg.selectAll("rect")
-.data(playerData)
-.enter()
-.append("rect")
-.attr('x', 100)
-.attr('y', 100)
-.attr('width', 20)
-.attr('height', 20)
-.style({
-  'fill': 'blue',
-  'stroke': 'white',
-  'stroke-width': 1,
-});
+var playerData = [
+{name:'chris', shirt: 'stripes/awsome', isTall: true}
+  // {name:'claire', shirt: 'sweater', isTall: false}
+  ];
 
-// make the player draggable
-player.call (d3.behavior
+  var player = svg.selectAll("rect")
+  .data(playerData)
+  .enter()
+  .append("rect")
+  .attr('x', 100)
+  .attr('y', 100)
+  .attr('width', 20)
+  .attr('height', 20)
+  .style({
+    'fill': 'blue',
+    'stroke': 'white',
+    'stroke-width': 1,
+  });
+
+// add drag behavior
+player.call(d3.behavior
  .drag()
- .on ('drag', function () {
+ .on('dragstart', function() {player.style('fill', 'grey');})
+ .on ('drag', function (d, i) {
    d3.select(this)
    .attr ({
      x : d3.event.x,
      y : d3.event.y
    });
- }));
+ }) 
+ .on('dragend', function() {player.style('fill', 'blue');})
+ );
+
+
+// Detect when a circle touches the player. (this only works at the end of eah path)
+
+var detectCollision = function () {
+  circles
+  .each (function (d, i) {
+
+    var x = Math.abs(circles[0][i].getAttribute('cx') - player.attr('x'));
+    var y = Math.abs(circles[0][i].getAttribute('cy') - player.attr('y'));
+    var distance = Math.sqrt((x * x) + (y * y));
+    var circleRadius = circles[0][i].getAttribute('r');
+    var playerRadius = Math.sqrt((player.attr('x')*player.attr('x')*(player.attr('y')*player.attr('y')) ));
+    if (distance <= circleRadius + playerRadius ) 
+    {
+      collision();
+    }
+  });
+};
+
+d3.timer(detectCollision, 5000);
+d3.timer(scoreBoard, 5000);
+
+
+// Track of the user's score and display it.
+
+var curCount = document.querySelector('.current span').innerText;
+
+function scoreBoard (){
+  
+  // handle current score
+  var tempCur = Number(curCount);
+  tempCur++;
+  curCount = tempCur;
+  document.querySelector('.current span').innerText = tempCur;
+
+}
+
+
+// handle collision score 
+var cCount = document.querySelector('.collisions span').innerText;
+
+function collision(){
+  
+  var temp = Number(cCount);
+  temp++;
+  cCount = temp;
+
+  document.querySelector('.collisions span').innerText = temp;
+  resetCurScore (); //reset current score to 0 on a collision
+}
+
+// handle high score and reset current score to 0 on a collision
+var highCount = document.querySelector('.high span').innerText;
+
+function resetCurScore (){
+  if (curCount > highCount){
+    highCount = document.querySelector('.high span').innerText = curCount;
+  }
+  curCount = document.querySelector('.current span').innerText = 0;
+}
